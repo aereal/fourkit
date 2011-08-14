54a561984008d65487cf6bfef3e6052675175f9f
AOKI Hanae <aereal@kerare.org>
2011-08-15 05:06:56 +0900

    add versions#show

--- app/controllers/versions.rb
+++ app/controllers/versions.rb
@@ -1,9 +1,11 @@
-WikiPs.controllers :versions, :map => 'pages/:title' do
+WikiPs.controllers :versions, :map => '/:title' do
 	get :index do
 		@page = Page.first(:title => params[:title], :order => :created_at.desc)
 		render 'versions/index'
 	end
 
-	get :show do
+	get :show, :map => ":id" do
+		@page = Page.find(params[:id])
+		render 'versions/show'
 	end
 end
