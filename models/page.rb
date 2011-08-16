bef67c190604169248ff00e3af0c4a6ca4bff479
AOKI Hanae <aereal@kerare.org>
2011-08-16 16:56:53 +0900

    separated version (revision) from model/page.rb

--- models/page.rb
+++ models/page.rb
@@ -1,26 +1,8 @@
 class Page
 	include MongoMapper::Document
-	plugin MongoMapper::Plugins::IdentityMap
 
 	# key <name>, <type>
-	key :title, String
-	key :body, String
-	key :parent_id, ObjectId
+	key  :title, String
+	many :revisions
 	timestamps!
-
-	belongs_to :parent, :class_name => :Page
-
-	scope :titles, all.uniq(&:title)
-
-	def versions
-		self.class.where(:title => self.title)
-	end
-
-	def children
-		self.class.where(:parent_id => self.id)
-	end
-
-	def descendant
-		self.children.map {|child| [child, child.descendant] }
-	end
 end
--- models/revision.rb
+++ models/revision.rb
@@ -1,7 +1,15 @@
 class Revision
-  include MongoMapper::Document
+	include MongoMapper::EmbeddedDocument
+	plugin MongoMapper::Plugins::IdentityMap
 
-  # key <name>, <type>
-  key :body, Body
-  timestamps!
+	# key <name>, <type>
+	key :parent_id, ObjectId
+	key :body, String
+	key :created_at, Time
+
+	belongs_to :parent, :class_name => :Revision
+
+	def children
+		self.class.where(parent_id: self.id)
+	end
 end
