0f40da8f09a240261ce78a4d34400bcb875a2226
AOKI Hanae <aereal@kerare.org>
2011-08-16 23:30:04 +0900

    verions -> revisions

--- app/controllers/revisions.rb
+++ app/controllers/revisions.rb
@@ -1,17 +1,19 @@
-WikiPs.controllers :versions, :map => '/:title' do
+WikiPs.controllers :revisions, :map => '/:title' do
 	get :index do
-		@page = Page.first(:title => params[:title], :order => :created_at.desc)
-		render 'versions/index'
+		@page = Page.find_by_title(params[:title])
+		render 'revisions/index'
 	end
 
 	get :show, :map => ":id" do
-		@page = Page.find(params[:id])
-		render 'versions/show'
+		@page = Page.find_by_title(params[:title])
+		@revision = @page.revisions.find(params[:id])
+		render 'revisions/show'
 	end
 
 	get :fork, :map => ':id/fork' do
-		@page = Page.find(params[:id])
-		render 'versions/fork'
+		@page = Page.find_by_title(params[:title])
+		@revision = @page.revisions.find(params[:id])
+		render 'revisions/fork'
 	end
 
 	post :create do
