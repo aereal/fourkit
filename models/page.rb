3cbd4c18d1ce02b0673578a90f49978202466d32
AOKI Hanae <aereal@kerare.org>
2011-08-16 17:42:52 +0900

    add Page#revise

--- models/page.rb
+++ models/page.rb
@@ -3,6 +3,13 @@ class Page
 
 	# key <name>, <type>
 	key  :title, String
-	many :revisions
+	many :revisions, class_name: 'Revision'
 	timestamps!
+
+	def revise(attrs)
+		Revision.new(attrs.merge(created_at: Time.now)).tap do |rev|
+			self.revisions << rev
+			self.save
+		end
+	end
 end
--- models/revision.rb
+++ models/revision.rb
@@ -1,15 +1,10 @@
 class Revision
 	include MongoMapper::EmbeddedDocument
-	plugin MongoMapper::Plugins::IdentityMap
 
 	# key <name>, <type>
-	key :parent_id, ObjectId
 	key :body, String
 	key :created_at, Time
 
-	belongs_to :parent, :class_name => :Revision
-
-	def children
-		self.class.where(parent_id: self.id)
-	end
+	embedded_in :page
+	belongs_to :parent, :class_name => 'Revision'
 end
